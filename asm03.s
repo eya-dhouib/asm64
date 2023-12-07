d656c1e85d7ee811deba50cd4673479187ce4332
eya-dhouib <eya.dhouib@sesame.com.tn>
2023-12-07 04:34:28 -0500

    03

---
+++ asm03.s
@@ -0,0 +1,30 @@
+section .data
+    pointeur dd '1337', 10
+section .bss
+    receveur resb 4;
+section .text
+    global _start
+
+_start:
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, receveur
+    mov rdx, 4
+    syscall
+
+    cmp byte [receveur], 0x34
+    je _error
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, pointeur
+    mov rdx, 5
+    syscall
+
+    mov rax, 60
+    mov rdi, 0
+    syscall
+_error:
+    mov rax, 60
+    mov rdi, 1
+    syscall
