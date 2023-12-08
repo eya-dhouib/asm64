4343aebf2548827466e8d65078454577d2cf545c
eya-dhouib <eya.dhouib@sesame.com.tn>
2023-12-08 09:31:29 -0500

    asm01 v2

--- asm01.s
+++ asm01.s
@@ -1,19 +1,7 @@
 bits 64
-
 section .text
 global _start
-
 _start:
-    ;push 0x37333331
-    mov r15, 0x0a37333331
-    push r15
-
-    mov al, 1
-    mov dil, 1
-    mov rsi, rsp
-    mov dl, 8
-    syscall
-
     mov al, 60
-    xor dil, dil ; xor rdi, rdi ; mov rdi, 0
+    mov rdi,rdi
     syscall
