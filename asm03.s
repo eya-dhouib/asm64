37dfce3149ee25ec76573b9878f03db477b1beaf
eya-dhouib <eya.dhouib@sesame.com.tn>
2023-12-14 05:01:09 -0500

    asm03

--- asm03.s
+++ asm03.s
@@ -6,25 +6,25 @@ section .text
     global _start
 
 _start:
-    mov rax, 0
+    mov rax, 0 ;read
     mov rdi, 0
     mov rsi, receveur
     mov rdx, 4
     syscall
 
     cmp byte [receveur], 0x34
-    je _error
+    je _1337
 
-    mov rax, 1
+    mov rax, 60
     mov rdi, 1
-    mov rsi, pointeur
-    mov rdx, 5
+    syscall
+_1337:
+    mov rax,1
+    mov rdi,1
+    mov rsi,pointeur
+    mov rdx,5
     syscall
 
     mov rax, 60
     mov rdi, 0
-    syscall
-_error:
-    mov rax, 60
-    mov rdi, 1
     syscall
