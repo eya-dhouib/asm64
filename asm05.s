d1b9aebe3b797f755d66203f803eff5ce86be0ae
eya-dhouib <63847829+eya-dhouib@users.noreply.github.com>
2024-01-14 20:57:38 +0100

    asm05.s v2

--- asm05.s
+++ asm05.s
@@ -1,28 +1,24 @@
-section .data
-    asm01_path db "./asm01", 0
-
 section .text
     global _start
 
 _start:
     ; Appel à execve
-    mov rdi, asm01_path  ; Pointeur vers le chemin du programme à exécuter
-    mov rsi, 0
-    mov rdx, 0
-    mov rax, 59          ; 59 pour executer
+    asm01_path dq "./asm01"
+    push asm01_path
+    xor rsi, rsi
+    xor rdx, rdx
+    mov al, 59       ; 59 pour executer
     syscall
 
-
-    cmp rax, 0
+    cmp rax, rsi
     jl  error_exit
 
-    ; Fin du programme asm05
-    mov rax, 60
+    mov al, 60
     xor rdi, rdi
     syscall
 
 error_exit:
-    ; Gestion des erreurs (à personnaliser selon vos besoins)
-    mov rax, 60
+    mov al, 60
     xor rdi, rdi
     syscall
+
